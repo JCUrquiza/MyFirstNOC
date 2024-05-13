b7b810a5de0f26d86ff906afa3fcc646ea015a07
Juan <gurquiza8@gmail.com>
2024-05-13 15:31:14 -0500

    Implementing cron library like a service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,18 @@
+import { CronService } from "./cron/cron-service";
 
 export class Server {
 
     public static start() {
 
         console.log('Server started...');
+
+        CronService.createJob(
+            '*/3 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('3 seconds: ', date);   
+            }
+        );
         
     }
 
