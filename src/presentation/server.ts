b092bcc964e770a0f7c1358f59e1102d1eb27cdb
Juan <gurquiza8@gmail.com>
2024-05-14 14:43:55 -0500

    save logs with success

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
-import { CheckService } from "../domain/use-cases/checks/check-service";
-import { CronService } from "./cron/cron-service";
+import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasources';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
+import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource() 
+);
 
 export class Server {
 
@@ -10,9 +16,10 @@ export class Server {
         CronService.createJob(
             '*/3 * * * * *',
             () => {
-                const url = 'http://localhost:3000';
+                const url = 'https://google.com';
                 // new CheckService().execute('https://google.com');
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(`${ url } is ok`),
                     ( error ) => console.log( error )
                 ).execute(url);
