7d732babb6414470d75fdb8a54ab8d77fcdd6467
Juan Carlos <gurquiza8@gmail.com>
2024-05-26 13:56:21 -0600

    Save and read in database with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import { envs } from './config/plugins/envs.plugin';
 import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
@@ -15,6 +16,20 @@ async function main() {
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME
     });
+
+    // const prisma = new PrismaClient();
+    // const newLog = await prisma.logModel.create({
+    //     data: {
+    //         level: 'HIGH',
+    //         message: 'Test message',
+    //         origin: 'App.ts'
+    //     }
+    // });
+    // console.log(newLog);
+
+    // const logs = await prisma.logModel.findMany();
+    // console.log(logs);
+
 
     // Crear una colección = tables, documento = registro
     // const newLog = await LogModel.create({
