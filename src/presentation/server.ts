baffd2795821c57e3d13687ccac73bc62d4814bd
Juan Carlos <gurquiza8@gmail.com>
2024-05-26 14:14:47 -0600

    Save and read from dbprostgres

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -3,13 +3,15 @@ import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasources';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
+import { PostgresLogDatasource } from '../infrastructure/datasources/postgres-log.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
 const logRepository = new LogRepositoryImpl(
     // new FileSystemDatasource(),
-    new MongoLogDatasource(),
+    // new MongoLogDatasource(),
+    new PostgresLogDatasource()
 );
 
 const emailService = new EmailService();
@@ -43,18 +45,18 @@ export class Server {
         //     `
         // }
 
-        // CronService.createJob(
-        //     '*/3 * * * * *',
-        //     () => {
-        //         const url = 'https://google.com';
-        //         // new CheckService().execute('https://google.com');
-        //         new CheckService(
-        //             logRepository,
-        //             () => console.log(`${ url } is ok`),
-        //             ( error ) => console.log( error )
-        //         ).execute(url);
-        //     }
-        // );
+        CronService.createJob(
+            '*/3 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                // new CheckService().execute('https://google.com');
+                new CheckService(
+                    logRepository,
+                    () => console.log(`${ url } is ok`),
+                    ( error ) => console.log( error )
+                ).execute(url);
+            }
+        );
         
     }
 
